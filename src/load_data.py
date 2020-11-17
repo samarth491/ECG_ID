e657e137b859971cba91f0dbaeda8cc2957520ff
samarth491 <setu.samarth@gmail.com>
2020-11-17 16:37:13 +0530

    Add functionality to return sampling freq while loading data

--- src/load_data.py
+++ src/load_data.py
@@ -1,11 +1,16 @@
 """
 This script is used to load the signal dataset from the data folder.
 The data extracted is in csv format for easy conversion into matrix.
+This script also gives the sampling frequency of data which is used
+in pre-processing steps. 
 """
 
 
 import os
+
 import numpy as np
+import wfdb as wf
+
 import CONSTANTS as const
 
 """
@@ -17,6 +22,7 @@ return: list of datasets
 def load_data():
     filenames = [file for file in os.listdir(const.CSV_PATH)]
     datasets = []
+    freqs = []
 
     for filename in filenames:
         dataset = np.genfromtxt(const.CSV_PATH+filename, 
@@ -24,4 +30,8 @@ def load_data():
                                 skip_header=1)
         datasets.append(dataset)
 
-    return datasets
+        record = wf.rdrecord(const.RAW_PATH+filename[:-4])
+        freqs.append(record.__dict__['fs'])
+
+    
+    return [freqs, datasets]
