b9644401470ea070cfda2285b8ce89db021f8a2c
samarth491 <setu.samarth@gmail.com>
2020-12-08 16:23:54 +0530

    Add a script that preprocesses the raw data This script is used to clean the data, find peaks and then segment the data. Then this data is sent to the model for training and testing.

---
+++ src/preprocess.py
@@ -0,0 +1,80 @@
+"""
+This script is used to preprocess the data before sending it to the
+model. We remove the noise from signals, find peaks and then segment
+the data into smaller frames. Finally we return those frames in form
+of a dictionary.
+"""
+
+import load_data
+import clean_signal
+import peak_detection
+
+
+"""
+This function is used to segment the signal for a particular person
+according to the peaks detected.
+params:
+    signal -> de-noised signal which we want to segment
+    peaks -> indices of detected peaks of tha same signals
+    slide -> decide if we want the segments as moving window or not
+return:
+    segmented data for a particular signal
+"""
+
+def segment(signal, peaks, slide=True):
+    n = len(peaks)
+    one_seg = 8 # no. of heartbeats in one segment
+    sample_window = 128 # no. of samples before and after the peaks
+
+    frames = []
+
+    if slide:
+        for i in range(0, n - one_seg + 1):
+            l = peaks[i]
+            r = peaks[i + one_seg - 1]
+            l = max(l - sample_window, 0)
+            r = min(r + sample_window, n-1)
+            frames.append(signal[l: r + 1])
+    else:
+        for i in range(0, n, one_seg):
+            if i + one_seg >= n:
+                break
+
+            l = peaks[i]
+            r = peaks[i + one_seg - 1]
+            l = max(l - sample_window, 0)
+            r = min(r + sample_window, n-1)
+            frames.append(signal[l: r + 1])
+
+    return frames
+
+
+"""
+This is the driver function which preprocess the raw data.
+return:
+    preprocessed data in form of a dictionary where key is the id of
+    the signal and value is the segmented data.
+"""
+
+def preprocess():
+    data = load_data.load_data()
+    freqs = data[0]
+    raw_signals = data[1]
+    ids = data[2]
+
+    signals = clean_signal.clean_signal(freqs, raw_signals)
+
+    preprocessed_data = {}
+
+    for idx in range(0, len(signals)):
+        signal = signals[idx]
+        freq = freqs[idx]
+
+        peaks = peak_detection.detect_peaks(signal, freq)
+
+        segmented_data = segment(signal, peaks)
+
+        preprocessed_data[ids[idx]] = segmented_data
+
+    return preprocessed_data
+
