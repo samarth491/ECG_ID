fc394e2010929ca0276112b3285875e5d02daad2
samarth491 <setu.samarth@gmail.com>
2021-01-14 00:39:50 +0530

    Delete the experimentation scripts The experimentation folder is being deleted as the project is almost finalized and there was not a lot of information in those scripts.

--- experimentation/CONSTANTS.py
+++
@@ -1,9 +0,0 @@
-# insert the name of the directory where raw data resides here
-DIR = "/home/samarth/Documents/BTP/ECG_Data/"
-
-# insert the database name here
-RAW_DATA = "nsrdb/"
-CSV_DATA = "nsrdb_csv/"
-
-RAW_PATH = DIR + RAW_DATA
-CSV_PATH = DIR + CSV_DATA
